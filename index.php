02690a4f282d06449e5ecd0acca9b33778f9e3f2
Anal Mahata <developer.anal@gmail.com>
2024-03-13 18:15:10 +0530

    feat: Implement dynamic page titles
    - Introduce dynamic page titles by using a variable in the main script (index.php) and updating the title tag dynamically in the header component (header.php). This allows for easy customization of page titles on different sections of the website.

--- index.php
+++ index.php
@@ -1,51 +1,11 @@
-<?php
-    // Include the configuration file
-    require_once('./config/config.php');
+<!----------| index.php |-------->
+<?php 
+    $pageTitle = "Bengal Coding Club - Empowering Coders, Fostering Innovation"; // Default page title
+    include './components/common/header.php'; 
 ?>
 
 
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta name="description"
-        content="Bengal Coding Club - Empowering Coders, Fostering Innovation. Join our coding community for learning and collaboration." />
-
-    <meta name="keywords"
-        content="Bengal Coding Club, coding club, West Bengal Coding Club, programming club, coding community, coding classes, coding workshops, learn to code, coding events, tech community, programming community, coding tutorials, coding resources, coding education, technology education, coding for beginners, coding for kids, coding for students, web development, software development, computer science, coding bootcamp, coding skills, coding projects, coding challenges, coding mentorship, coding networking, coding meetups, coding forums, coding discussions" />
-
-    <meta name="author" content="Bengal Coding Club" />
-    <meta name="robots" content="index, follow" />
-    <meta property="og:title" content="Bengal Coding Club" />
-    <meta property="og:description"
-        content="Join Bengal Coding Club for a collaborative coding experience. Empowering coders and fostering innovation." />
-
-    <title>Bengal Coding Club - Empowering Coders, Fostering Innovation</title>
-
-    <!-----------<=====| Title icon |=====>----------->
-    <link rel="icon" type="image/x-icon" href="./assets/images/logos/main-favicon.ico" />
-
-    <!-----------<=====| Font awsome icon |=====>----------->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
-        integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-    <!-----------<=====| Stylesheets |=====>----------->
-    <link rel="stylesheet" href="./stylesheets/css/style.css" />
-
-    <!-------------------<[😎]>>-------------------
-        This website created by Anal Mahata
-
-        For any enquiry or collaboration ----<>
-            Email - developer.anal@gmail.com
-
-        See all my projects ----<>
-            Github - https://github.com/analmahata
-    --------------------<<[🙂]>------------------->
-</head>
-
-<body></body>
+</body>
 
 </html>
