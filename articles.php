edd5be086d5eb7a0bfa24b11f0cd480943c9d76a
Anal Mahata <developer.anal@gmail.com>
2024-03-21 15:14:36 +0530

    chore: Add articles.php to display latest updates
    - Created a new PHP file, articles.php, to display the latest updates section.
    - Incorporated articles with titles, excerpts, and author information.
    - Designed a responsive layout for optimal viewing across devices.
    - Included header.php and footer.php for consistent navigation and page structure.

---
+++ articles.php
@@ -0,0 +1,199 @@
+<?php 
+    $pageTitle = "Bengal Coding Club - Empowering Coders, Fostering Innovation";
+    include './components/common/header.php'; 
+?>
+
+
+<main class="responsive-wrapper">
+    <div class="page-title">
+        <h1>Latest Updates</h1>
+    </div>
+    <div class="magazine-layout">
+        <div class="magazine-column">
+            <article class="article">
+                <h2 class="article-title article-title--large">
+                    <a href="https://indianexpress.com/article/explained/explained-sci-tech/meet-devin-ai-first-autonomous-ai-software-engineer-9212537/"
+                        class="article-link">Meet <mark class="mark mark--primary">Devin AI,</mark>
+                        the world’s ‘first fully autonomous’ AI software engineer</a>
+                </h2>
+                <div class="article-excerpt">
+                    <p>
+                        Devin comes with some advanced capabilities in software development, including coding,
+                        debugging, problem-solving, etc.
+                    </p>
+                </div>
+                <div class="article-author">
+                    <div class="article-author-info">
+                        <dl>
+                            <dt><a href="https://indianexpress.com/profile/author/bijin-jose/">Bijin Jose</a></dt>
+                            <dd>Assistant Editor at Indian Express Online in New Delhi</dd>
+                        </dl>
+                    </div>
+                </div>
+            </article>
+            <article class="article">
+                <figure class="article-img-container">
+                    <img class="article-img" src="./assets/images/medical-banner-with-doctor-wearing-mask.jpg" />
+                </figure>
+                <h2 class="article-title article-title--medium">
+                    <a href="https://indianexpress.com/article/technology/artificial-intelligence/google-health-apollo-radiology-international-collaborate-9223098/"
+                        class="article-link">Google Health, <mark class="mark mark--secondary">Apollo
+                            collaborate
+                            to harness AI</mark> for early disease
+                        detection in India</a>
+                </h2>
+
+                <div class="article-excerpt">
+                    <p>
+                        In its collaboration with Apollo Radiology International in India, Google is building on AI
+                        advancements in healthcare to make them accessible across the nation.
+                    </p>
+                </div>
+                <div class="article-author">
+                    <div class="article-author-info">
+                        <dl>
+                            <dt><a href="https://indianexpress.com/profile/author/bijin-jose/">Bijin Jose</a></dt>
+                            <dd>Assistant Editor at Indian Express Online in New Delhi</dd>
+                        </dl>
+                    </div>
+                </div>
+            </article>
+        </div>
+        <div class="magazine-column">
+            <article class="article">
+                <figure class="article-img-container">
+                    <img class="article-img" src="./assets/images/ai-cloud-concept-with-robot-arm.jpg" />
+                </figure>
+                <h2 class="article-title article-title--medium">
+                    <a href="https://www.brookings.edu/articles/how-artificial-intelligence-is-transforming-the-world/"
+                        class="article-link">How artificial intelligence is transforming the world</a>
+                </h2>
+                <div class="article-excerpt">
+                    <p>
+                        Artificial intelligence (AI) is a wide-ranging tool that enables people to rethink how we
+                        integrate information, analyze data, and use the resulting insights to improve decision
+                        making—and already it is transforming every walk of life. In this report, Darrell West and John
+                        Allen discuss AI’s application across a variety of sectors, address issues in its development,
+                        and offer recommendations for getting the most out of AI while still protecting important human
+                        values...
+                    </p>
+                </div>
+                <div class="article-author">
+                    <div class="article-author-info">
+                        <dl>
+                            <dt>By <a href="https://www.brookings.edu/people/darrell-m-west/">Darrell M. West</a> and
+                                <a href="https://www.brookings.edu/people/john-r-allen/">John R. Allen</a>
+                            </dt>
+                            <dd>April 24, 2018</dd>
+                        </dl>
+                    </div>
+                </div>
+            </article>
+        </div>
+        <div class="magazine-column">
+            <article class="article">
+                <figure class="article-img-container">
+                    <img class="article-img" src="./assets/images/modern-equipped-computer-lab.jpg" />
+                </figure>
+                <h2 class="article-title article-title--small">
+                    <a href="https://theconversation.com/ai-can-now-attend-a-meeting-and-write-code-for-you-heres-why-you-should-be-cautious-217733"
+                        class="article-link">AI can now <mark class="mark mark--secondary">attend a
+                            meeting</mark>,
+                        and write code for you – here’s why you should be cautious</a>
+                </h2>
+                <div class="article-creditation">
+                    <p>By Simon Thorne</p>
+                </div>
+            </article>
+            <article class="article">
+                <figure class="article-img-container">
+                    <img class="article-img" src="./assets/images/opened-ai-chat-laptop.jpg" />
+                </figure>
+
+                <h2 class="article-title article-title--medium">
+                    <a href="#" class="article-link">New Research Exposes <mark class="mark mark--tertiary">Security
+                            Risks in ChatGPT</mark> Plugins </a>
+                </h2>
+
+                <div class="article-excerpt">
+                    <p>
+                        Security researchers have uncovered critical security flaws within ChatGPT plugins. By
+                        exploiting these flaws, attackers could seize control of an organization’s account on
+                        third-party platforms and access sensitive user data, including Personal Identifiable
+                        Information (PII)...
+                    </p>
+                </div>
+
+                <div class="article-author">
+                    <div class="article-author-info">
+                        <dl>
+                            <dt> By <a
+                                    href="https://www.infosecurity-magazine.com/profile/alessandro-mascellino/">Alessandro
+                                    Mascellino</a></dt>
+                            <dd>Freelance Journalist</dd>
+                        </dl>
+                    </div>
+                </div>
+            </article>
+        </div>
+        <div class="magazine-column">
+            <article class="article">
+                <h2 class="article-title article-title--medium">
+                    <a href="https://www.outsourceaccelerator.com/articles/programming-trends/" class="article-link">Top
+                        16 programming trends to look out for in 2024</a>
+                </h2>
+                <div class="article-excerpt">
+                    <p>
+                        Programming trends change as developers keep working on newer languages to innovate apps,
+                        websites, games, and infrastructures.
+                    </p>
+
+                    <p>
+                        It’ll be hard to keep up with these trends but companies, businesses, and individuals have to
+                        step up. Especially when the industry is not so kind to those who fall behind...
+                    </p>
+                </div>
+                <div class="article-author">
+                    <div class="article-author-info">
+                        <dl>
+                            <dt>By Derek Gallimore</dt>
+                        </dl>
+                    </div>
+                </div>
+            </article>
+            <article class="article">
+                <small class="article-category">
+                </small>
+                <h2 class="article-title article-title--medium">
+                    <a href="#" class="article-link">Researchers Uncover New <mark
+                            class="mark mark--primary">“Conversation Overflow”</mark> Tactics </a>
+                </h2>
+
+                <div class="article-excerpt">
+                    <p>
+                        Threat researchers have revealed a new cyber-attack using cloaked emails to deceive machine
+                        learning (ML) systems, enabling the infiltration of enterprise networks...
+                    </p>
+                </div>
+
+                <div class="article-author">
+                    <div class="article-author-info">
+                        <dl>
+                            <dt> By <a
+                                    href="https://www.infosecurity-magazine.com/profile/alessandro-mascellino/">Alessandro
+                                    Mascellino</a></dt>
+                            <dd>Freelance Journalist</dd>
+                        </dl>
+                    </div>
+                </div>
+            </article>
+        </div>
+    </div>
+</main>
+
+
+
+<!----------| Import footer component |---------->
+<?php 
+    include './components/common/footer.php'; 
+?>
